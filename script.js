17d8e10495c2c596867666c2e076c1354aad3a54
Dmitry Kulikov <dak129@mail.ru>
2025-06-27 21:40:50 +0400

    Add count displaced

--- script.js
+++ script.js
@@ -31,8 +31,8 @@ const changeFigure = (event) => {
     figures[index].style.backgroundColor = circles[index].changeBcolor();
 }
 
-const generatePuzzle = (numOfColors) => {
-    let arr = [];
+const generatePuzzle = () => {
+    const arr = [];
     for (let i = 0; i < circles.length; i++) {
         let colorIndex = Math.floor(Math.random() * numOfColors);
         arr.push(colorIndex);
@@ -41,7 +41,7 @@ const generatePuzzle = (numOfColors) => {
 };
 
 const getCurrentColors = () => {
-    let arr = [];
+    const arr = [];
     for (let i = 0; i < circles.length; i++) {
         arr.push(Circle.bcolors.indexOf(circles[i].bcolor));
     }
@@ -56,16 +56,48 @@ const checkMatches = (curGuess) => {
         }
     }
     return countExact;
-}  
+} 
+
+const countUnmatched = (curGuess) => {
+    const curUnmatched = [];
+    const puzzleUnmatched = [];
+    for (let i = 0; i < circles.length; i++) { 
+        if (curGuess[i] != puzzle[i]) {
+            curUnmatched.push(curGuess[i]);
+            puzzleUnmatched.push(puzzle[i]);
+        }
+    }
+    let count = 0;
+    for (let i = 0; i < puzzleUnmatched.length; i++) {
+        if (curUnmatched.includes(puzzleUnmatched[i])) {
+            count++;
+        }
+    }
+    return count;
+} 
+
+const getColorOccurrences = (curGuess) => {
+    const colorOccurrences = new Array(numOfColors).fill(0);
+    for (let i = 0; i < circles.length; i++) {
+        colorOccurrences[curGuess[i]]++;
+    }
+    return colorOccurrences;
+}
 
 const guess = () => {
     countGuesses++;
     counter.value = "Попытка " + countGuesses.toString();
     const myGuess = getCurrentColors();
     const exactMatches = checkMatches(myGuess);
+    /* const myOccurences = getColorOccurrences(myGuess);
+    const puzzleOccurences = getColorOccurrences(puzzle); */
+    const unmatched = countUnmatched(myGuess);
     answer.innerText =  myGuess.join(' ') + ' vs ' 
                         + puzzle.join(' ') + ' exact ' 
-                        + exactMatches.toString();
+                        + exactMatches.toString() + ' displaced ' 
+                        + unmatched.toString(); 
+                        /* + ' ocs '  + myOccurences.join(' ') 
+                        + ' true ocs' + puzzleOccurences.join(' ') */
 }
 
 const circleContainer = document.getElementById("circle-container");
@@ -77,7 +109,7 @@ const circles = [new Circle(0), new Circle(1),
                  new Circle(2), new Circle(3)];
 
 const numOfColors = Circle.bcolors.length;
-const puzzle = generatePuzzle(numOfColors);
+const puzzle = generatePuzzle();
 
 const figures = [document.getElementById("circle0"),
                  document.getElementById("circle1"),
